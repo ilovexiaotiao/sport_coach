e3f8ccd5b0dc2480e256970bc4a43d2f1f23117c
ilovexiaotiao <18688989936@163.com>
2018-12-29 17:13:24 +0800

    啊啊

--- apprun.py
+++ apprun.py
@@ -1,80 +1,111 @@
 # encoding=utf8
 
 import os
-from flask import Flask, request, redirect, url_for,send_from_directory,render_template,make_response,jsonify
-from werkzeug import secure_filename
-import urllib,urllib2
-import json,os,datetime
-import redis
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template, make_response, jsonify
+# from werkzeug import secure_filename
+import urllib, urllib2
+import json, os, datetime
+# import redis
 import collections
 
 app = Flask(__name__)
-path = "E:\\sport_coach\\static\\json\\lesson_list1.json"
-with open(path,"r") as f:
+path = "D:\\sport_coach\\static\\json\\lesson_list1.json"
+with open(path, "r") as f:
     # file = file.decode("utf-8-sig")
     data = json.load(f)
 
 
+# 获取课程JSON数据
 class Alist(object):
-    def __init__(self,index,datas,datelist):
-        result_sorted=collections.OrderedDict()
+    def __init__(self, index, datas, datelist):
+        # 定义顺序字典
+        result_sorted = collections.OrderedDict()
+        # 定义课程类别序号
         self.index = index
+        # 定义课程大类名称
         self.indexname = datas[index]["name"]
+        # 定义课程大类描述
         self.indexdesc = datas[index]["desc"]
-        #print self.indexname
+        # print self.indexname
+        # 获取大类下的课程明细JSON
         result = datas.get(index)["arrange"]
-        key_list = result.keys()
-        key_list.sort()
-        #print key_list
-        result_sort = result_sorted.fromkeys(key_list)
-        #print result_sort
-            # 循环赋值
-        for i in range(0,len(key_list)):
-            if key_list[i] in datelist:
-                print key_list[i]
-                result_sort[key_list[i]] = result.get(key_list[i])
-            self.result_sorted = result_sort
+        # key_list = result.keys()
+        # key_list.sort()
+        # print key_list
+        # 参考datalist，生成有序字典
+        result_sort = result_sorted.fromkeys(datelist)
+        # print result_sort
+        # 循环赋值
+        for i in range(0, len(datelist)):
+            # if datelist[i] in key_list:
+            # print key_list[i]
+            result_sort[datelist[i]] = result.get(datelist[i])
+        # print result_sort
+        self.result_sorted = result_sort
+
 
 class Adate(object):
     def __init__(self):
         self.date = datetime.date.today() + datetime.timedelta(days=1)
 
-
-    def time_string(self,i):
+    def time_string(self, i):
         date1 = self.date + datetime.timedelta(days=i)
         return date1.strftime(
             '%a')
 
-    def get_date_list(self):
+    def date_string(self, i):
+        date1 = self.date + datetime.timedelta(days=i)
+        return date1.strftime('%m月%d日')
+
+    # 获取日期字典
+    def get_date_dict(self):
+        dic = {}
         list = []
-        for i in range(0,3):
+        for i in range(0, 3):
             list.append(self.time_string(i))
-        return list
+        dic.fromkeys(list)
+        for i in range(0, len(list)):
+            dic[list[i]] = self.date_string(i).decode('utf-8')
+        return dic
 
 
+# datee = Adate()
+# datelist=datee.get_date_dict().keys()
+# print datelist
+# dataa= Alist("1",data,datelist)
+# dictt = dataa.result_sorted
+# for key,value in dictt.items():
+#     for i in value:
+#         print i['end']
+#
+# print dataa.result_sorted.values()
 
 
-@app.route('/lesson_list',methods = ['POST', 'GET'])
+@app.route('/lesson_list', methods=['POST', 'GET'])
 def get_list():
     if request.method == 'POST':
         strings = request.get_json()
-        indexname=strings['index']
+        indexname = strings['index']
         dates = Adate()
-        datelist = dates.get_date_list()
+        datedict = dates.get_date_dict()
+        datelist = datedict.keys()
+        # print datedict
         list = Alist(indexname, data, datelist)
-        print list.result_sorted
-        resp=make_response()
+        # print type(list.result_sorted.values())
+        resp = make_response()
         resp.status_code = 200
         resp.headers["content-type"] = "text/html"
-        resp.response = render_template('lesson_list_content.html',result= list.result_sorted,indexname=list.indexname,desc=list.indexdesc,datelist = datelist)
-        #return jsonify(list.result)
+        resp.response = render_template('lesson_list_content.html', result=list.result_sorted, indexname=list.indexname,
+                                        desc=list.indexdesc, datedict=datedict)
+        # print resp.response
+        # return jsonify(list.result)
         return resp
     else:
         sorted_result = sorted(data.items(), key=lambda x: x[1], reverse=False)
         sorted_result = sorted_result[0][1]["arrange"]
-    return render_template('lesson_list.html',result= sorted_result )
+    return render_template('lesson_list.html', result=sorted_result)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    #app.run(debug=True)
+    # app.run(debug=True)
